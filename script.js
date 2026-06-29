97cf7704b74972d6c27b07f6a55e73f0f93f7a09
eslampop235-dot <eslam.pop235@gmail.com>
2026-06-29 20:33:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function shareCard() {
 
+    if (navigator.share) {
+
+        navigator.share({
+            title: 'Neveen Elshaarawy',
+            text: 'CEO - ONYX Global Logistics',
+            url: window.location.href
+        });
+
+    } else {
+
+        navigator.clipboard.writeText(window.location.href);
+
+        alert("Business Card Link Copied Successfully");
+
+    }
+
+}
